c597d45a7d6d4b5c06f24b0ac56da0293d6ba55e
qianlima8888 <1058088476@qq.com>
2019-01-16 09:31:29 +0800

    http页面元素提取
    根据需要提取标签

---
+++ html提取元素/提取 - 副本.cpp
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<string>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int htmljiexi(string html)
+{
+	string user,pinglun,dianzan;const char *qq;
+	int star=1,end;
+	user=html;
+	while(star>0)
+	{
+		qq=strstr(user.c_str(),"f-name");
+		if(qq==NULL)
+			break;
+		else
+			user=qq;
+		int i,t;
+		i=user.find("浏览");
+		t=user.find("f-info");
+		if(i>t)
+		{
+			user=strstr(user.c_str(),"f-info");
+			continue;
+		}
+		//用户名
+		cout<<"用户名：";
+		star=user.find("\>")+1;
+		end=user.find("\<");
+		while(star<end)
+			cout<<user[star++];
+		cout<<endl;
+		//发布时间
+		user=strstr(user.c_str(),"ui-mr8");
+		star=user.find("\>")+1;
+		end=user.find("\<");
+		cout<<"  发布时间:";
+		while(star<end)
+			cout<<user[star++];
+		cout<<endl;
+		//浏览
+		star=user.find("浏览");
+			end=star;
+			while(user[end]!='<')
+			{
+				//cout<<star<<endl;
+				pinglun+=user[end];
+				end++;
+			}
+			if((end-star)==4)
+					pinglun="浏览（0）";
+		//发布正文
+		user=strstr(user.c_str(),"f-info");
+		star=user.find("\>")+1;
+		end=user.find("\<");
+		if(star==end)
+		{
+			cout<<"内容为链接或图片,暂时无法显示\n";
+			goto loop;
+			continue;
+		}
+		//cout<<"内容为:";
+		while(star<end)
+			cout<<user[star++];
+		cout<<endl;
+loop:
+		{
+			cout<<"-------------------------------------"<<endl;
+			cout<<"    ";
+			//浏览数
+			cout<<pinglun;
+				pinglun="";
+				cout<<"    ";
+			//评论数
+			star=user.find("评论");
+			end=star;
+			while(user[end]!='<')
+			{
+				//cout<<star<<endl;
+				pinglun+=user[end];
+				end++;
+			}
+			if((end-star)==4)
+					cout<<"评论（0）";
+				else
+					cout<<pinglun;
+				pinglun="";
+			cout<<"    ";
+			//点赞数
+			star=user.find("赞");
+			end=star;
+			while(user[end]!='<')
+			{
+				//cout<<star<<endl;
+				dianzan+=user[end];
+				end++;
+			}
+			if((end-star)==2)
+					cout<<"赞（0）";
+				else
+					cout<<dianzan;
+				dianzan="";	
+		}
+		cout<<endl<<endl;
+	}
+	return 0;
+}
+
+int main()
+{
+	FILE *fp;
+	string html;
+	fp=fopen("d:\\zone.txt","r");
+	char ch;
+	ch=getc(fp);
+	while(ch!=EOF)
+	{
+		html+=ch;
+		ch=getc(fp);
+	}
+	htmljiexi(html);
+	return 0;
+}
